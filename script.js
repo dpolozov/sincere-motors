f65f797952c67db0928827ee4835ce5d1e07b1c5
dpolozov <dpolozov@gmail.com>
2026-03-24 18:33:38 -0700

    and sandwich for mobile menu and navSlide function

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('#burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Burger Animation (Optional: makes it turn into an 'X')
+        burger.classList.toggle('toggle');
+    });
+
+    // Close menu when a link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('nav-active');
+        });
+    });
+}
+
+navSlide();
